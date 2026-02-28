c131369002dcf22acc47ee77deb992a13b858d9f
Libin2004 <libinlibinlibin10@gmail.com>
2026-02-28 08:18:41 +0000

    first commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.40"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.25"
     }
   }
 
@@ -32,8 +32,5 @@ terraform {
     region = "us-east-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = ">= 1.6.0, < 2.0.0"
 }
-##
-##
-##
